73c93099dab1b30f0be3246023a5292b0f72d568
Prashant Soni <163483462+Prashants57@users.noreply.github.com>
2024-09-13 16:12:24 +0000

    datatype changes

---
+++ js_tutorial/datatype.js
@@ -0,0 +1,11 @@
+let nameOne = "Prashant Soni"
+let numbers = 123456789
+let booleanValue = true
+let nullData = null
+let undefineValue = undefined
+
+console.log(nameOne);
+console.log(numbers);
+console.log(booleanValue);
+console.log(nullData);
+console.log(undefineValue);
