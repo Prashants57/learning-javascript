b4b8a10dcb1b34e1b254bd1ab810f09c2006363e
Prashant Soni <163483462+Prashants57@users.noreply.github.com>
2024-09-14 01:59:43 +0000

    data type conversion

---
+++ js_tutorial/dataTypeConversion.js
@@ -0,0 +1,13 @@
+/* ----------------- data type conversion -------------------*/
+
+//---------- Numbers to strings
+
+let a = "3.14"
+console.log(a);
+console.log(Number(a));
+
+// conver empty in double inverted commas
+let b = ""
+
+console.log(Number(b))
+
