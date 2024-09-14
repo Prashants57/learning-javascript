13f7ad704bcc4ce406b88843cd157c088f508f48
Prashant Soni <163483462+Prashants57@users.noreply.github.com>
2024-09-14 01:26:28 +0000

    typeof  method use example

---
+++ js_tutorial/typeofUse.js
@@ -0,0 +1,11 @@
+let nameOne = "Prashant Soni"
+let numbers = 123456789
+let booleanValue = true
+let nullData = null
+let undefineValue = undefined
+
+console.log(typeof(nameOne));
+console.log(typeof(number));
+console.log(typeof(booleanValue));
+console.log(typeof(nullData));  //data type is object
+console.log(typeof(undefineValue));
