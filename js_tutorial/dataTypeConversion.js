ff2dacfe42c68fed06ae81f9f82ad6d61b4250cc
Prashant Soni <163483462+Prashants57@users.noreply.github.com>
2024-09-14 07:03:42 +0000

    unary operator

--- js_tutorial/dataTypeConversion.js
+++ js_tutorial/dataTypeConversion.js
@@ -2,12 +2,20 @@
 
 //---------- Numbers to strings
 
-let a = "3.14"
-console.log(a);
-console.log(Number(a));
+// let a = "3.14"
+// console.log(a);
+// console.log(Number(a));
 
-// conver empty in double inverted commas
-let b = ""
+//----- conver empty in double inverted commas
+// let b = ""
 
-console.log(Number(b))
+// console.log(Number(b))
+
+// unary operator use
+let a1 = "5"
+let ab = + a1;
+// console.log(ab);
+console.log(typeof(a1)); //show string 
+console.log(typeof(ab)); //show number
+
 
