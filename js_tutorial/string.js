5a41797c1db3ca08cda60a54897646659d2809e4
Prashant Soni <163483462+Prashants57@users.noreply.github.com>
2024-09-23 07:05:28 +0000

    condition in string

---
+++ js_tutorial/string.js
@@ -0,0 +1,29 @@
+// The string object is used for represent and manipulate the sequence of characters 
+
+//string are useful for holding data that can be represented in text form
+
+let textFirst = "This is first demo string, using for testing purpose." //representing string in double or single quote
+
+// console.log(textFirst)
+
+let textSecond =  new String('this is demo second string')
+
+// console.log(textSecond);
+
+//--------------- comparing string
+
+const a = "A"
+const b = "b"
+
+if(a < b){
+    console.log(`${a} is less than ${b}`);
+}
+else if(a > b){
+    console.log(`${a} is grater than ${b}`);
+}
+else{
+    console.log(`${a} and ${b} are equal`);
+}
+
+
+
