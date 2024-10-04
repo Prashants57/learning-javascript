6f2d81a7ca4b47992e06bb468c7e0b3d2badd904
Prashant Soni <163483462+Prashants57@users.noreply.github.com>
2024-10-04 00:24:05 +0000

    charAt method

---
+++ js_tutorial/strings_methods.js
@@ -0,0 +1,7 @@
+let sentence = "This is demo texts for using string methods";
+let index = 5;
+
+// charAt() for return character specified index(position) in a string
+
+console.log(`The charector at index ${index} is ${sentence.charAt(index)}`);
+
