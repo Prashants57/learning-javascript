3f25dcd1dbcc5d50a14d6cb60c76d866fa4d6488
Prashant Soni <163483462+Prashants57@users.noreply.github.com>
2024-11-09 01:17:48 +0000

    learn new js method

---
+++ js_tutorial/string/charAtMethod.js
@@ -0,0 +1,5 @@
+const characters = "A human can not fly without feather";
+
+const index = 0;
+
+console.log(`In this characters at index ${index} is sentence ${characters.charAt(index)}`);
---
+++ js_tutorial/string/charCodeAt.js
@@ -0,0 +1,3 @@
+const sentences = "This is simple charecters from the sentence"
+const index = 10;
+console.log(`this example of -----${sentences.charCodeAt(index)} this is ${sentences.charAt(index)}`);
